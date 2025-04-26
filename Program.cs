2a6262200b21e9eeba20722e8d8be9adc886f7e1
Nooby Sandwich <avin.workspace@gmail.com>
2025-04-26 23:01:44 +0530

    chore: scaffold WebApp with Identity & EF Core

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TrackerWebApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add DB context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add MVC + Razor
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+// Configure middleware (error pages, static files, routing)
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
